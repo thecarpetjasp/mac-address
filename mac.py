281fae54f4ff00e4b207f0d2e5aaa9256319983b
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2020-09-27 04:42:35 +0000

    Update mac.py

--- mac.py
+++ mac.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import colorama
 import mac_vendor_lookup
+from colorama import Fore, Style
 from mac_vendor_lookup import MacLookup
 print ("")
-mac_add = input('MAC ADDRESS: ')
-print ("")
-print ("**********")
-print (MacLookup().lookup(mac_add))
-print ("**********")
+mac_add = input('MAC ADDRESS: ' + Fore.YELLOW)
+print (Style.RESET_ALL)
+print ("******************************")
+print (Fore.YELLOW)
+print ("{", MacLookup().lookup(mac_add), "}", sep="")
+print (Style.RESET_ALL)
+print ("******************************")
