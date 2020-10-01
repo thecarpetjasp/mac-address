6da6055ebe396243fec76bdbcfec965306035420
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2020-10-01 16:01:22 +0100

    Add files via upload

---
+++ mac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import colorama
+import mac_vendor_lookup
+from colorama import Fore, Style
+from mac_vendor_lookup import MacLookup
+
+while True:
+    try:
+        print ("")
+        mac_add = input('MAC ADDRESS: ' + Fore.YELLOW)
+        print (Style.RESET_ALL)
+        print ("******************************")
+        print (Fore.YELLOW)
+        print ("{", MacLookup().lookup(mac_add), "}", sep="")
+        print (Style.RESET_ALL)
+        print ("******************************")
+        break
+    except (mac_vendor_lookup.InvalidMacError, KeyError):
+        print ("SORRY, NO RESULTS")
+        print (Style.RESET_ALL)
+        print ("******************************")
+        break
---
+++ runme.py
@@ -0,0 +1,12 @@
+import os
+import colorama
+from colorama import Fore, Style
+
+os.system("cls||sudo chmod +x mac.py")
+os.system("cls||sudo mv mac.py /usr/bin/mac")
+os.system("cls||clear")
+
+print ("Setup complete.")
+print ("Type", Fore.YELLOW + "mac", Style.RESET_ALL + "anywhere in your terminal to run this program.")
+print ("")
+print (Fore.GREEN + "You can now delete this folder")
