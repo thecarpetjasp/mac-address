7f7141feeca3e2b7dca06a33679e4bb9325ec8aa
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2021-04-07 19:36:04 -0400

    Update runme.py

--- runme.py
+++ runme.py
@@ -3,10 +3,10 @@ import os
 import colorama
 from colorama import Fore, Style
 
-os.system("cls||sudo pip3 install -r requirements.txt")
-os.system("cls||sudo chmod +x mac.py")
-os.system("cls||sudo mv mac.py /usr/bin/mac")
-os.system("cls||clear")
+os.system("sudo pip3 install -r requirements.txt")
+os.system("sudo chmod +x mac.py")
+os.system("sudo mv mac.py /usr/bin/mac")
+os.system("clear")
 
 print ("Setup complete.")
 print ("Type", Fore.YELLOW + "mac", Style.RESET_ALL + "anywhere in your terminal to run this program.")
